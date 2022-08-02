fa60d698a12490f55dfcab33584626d2908ca05c
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-08-02 13:29:14 -0400

    Create overriding the data type of a column.py

---
+++ using csv/overriding the data type of a column.py
@@ -0,0 +1,4 @@
+pd.read_csv('aug_train.csv',dtype={'target':int}).info()
+#mention thecloumn whose data tyep we want to overrise as the key with the datatype we want as the value
+
+# {'column_name': dtype}
